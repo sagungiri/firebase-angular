245876f0d30744c91c8da64b937a47e19128e08b
sagungiri <gireesagun998@gmail.com>
2021-02-16 18:40:29 +0545

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { FirebaseService } from './services/firebase.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { SignInSignUpComponent } from './sign-in-sign-up/sign-in-sign-up.component';
+import { auth } from './auth/auth';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignInSignUpComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(auth)
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
